f0bec2d19cdcab3e88ed6dd39367996924b68e0c
marianeski <142414788+marianeski@users.noreply.github.com>
2025-08-07 15:15:42 +0000

    script

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const produtos = [];
 
 function adicionarProduto(){
-    
+    const nome = document.getElementById('nome').value;
+    const marca = document.getElementById('marca').value;
+    const preco = document.getElementById('preco').value;
+    const foto = document.getElementById('foto').value;
+    const descricao = document.getElementById('descricao').value;
+
+    const produto = {nome, marca, preco, foto, descricao};
+
+    produtos.push(produto);
 }
